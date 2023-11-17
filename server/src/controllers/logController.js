e00e7f7259f7e2a3f112d1527cff9f387bb3205d
Farhan Khan <farhaankhaan930@gmail.com>
2023-11-18 00:31:32 +0530

    defined logController methods

--- server/src/controllers/logController.js
+++ server/src/controllers/logController.js
@@ -1,15 +1,49 @@
+const Log = require("../models/logModel");
+
 const createLog = async (req, res) => {
     try {
+        const logData = req.body;
+        const newLog = new Log(logData);
+        await newLog.save();
 
+        res.status(200).send('Log added successfully');
     } catch (error) {
-
+        console.error('Error adding new log:', error);
+        res.status(500).json({ message: 'Error adding new log' });
     }
 };
 const getLog = async (req, res) => {
     try {
+        const { level, message, resourceId, startTimestamp, endTimestamp, traceId, spanId, commit, parentResourceId, regex } = req.query;
 
+        // Query based on the parameters
+        const query = {};
+
+        if (level) query.level = level;
+        if (message) {
+            query.message = { $regex: ".*"+message+".*", $options: 'i' };
+        }
+        if (resourceId) query.resourceId = resourceId;
+        if (traceId) query.traceId = traceId;
+        if (spanId) query.spanId = spanId;
+        if (commit) query.commit = commit;
+        if (parentResourceId) query['metadata.parentResourceId'] = parentResourceId;
+
+        // Timestamp included in query if required
+        if (startTimestamp && endTimestamp) {
+            query.timestamp = { $gte: new Date(startTimestamp), $lte: new Date(endTimestamp) };
+        } else if (startTimestamp) {
+            query.timestamp = { $gte: new Date(startTimestamp), $lte: new Date() };
+        } else if (endTimestamp) {
+            query.timestamp = { $gte: new Date(), $lte: new Date(endTimestamp) };
+        }
+
+        const filteredLogs = await Log.find(query).exec();
+
+        res.json(filteredLogs);
     } catch (error) {
-
+        console.error('Error getting logs:', error);
+        res.status(500).json({ message: 'Error getting logs' });
     }
 };
 
