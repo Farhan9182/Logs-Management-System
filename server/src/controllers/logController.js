5da6509209cc8ec59c83660ea08f124459f65322
Farhan Khan <farhaankhaan930@gmail.com>
2023-11-17 23:15:55 +0530

    declared log controllers, for POST and GET reqs

---
+++ server/src/controllers/logController.js
@@ -0,0 +1,16 @@
+const createLog = async (req, res) => {
+    try {
+
+    } catch (error) {
+
+    }
+};
+const getLog = async (req, res) => {
+    try {
+
+    } catch (error) {
+
+    }
+};
+
+module.exports = {getLog, createLog};
--- server/src/routes/logs.js
+++ server/src/routes/logs.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const { createLog, getLog } = require('../controllers/logController');
 
-// Login route
+// Log route
 router.post('/log', createLog);
 router.get('/log', getLog);
 
