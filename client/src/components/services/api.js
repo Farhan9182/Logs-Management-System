f8c98aae7bd580602d5f49b1a17dedd2c8430a75
Farhan Khan <farhaankhaan930@gmail.com>
2023-11-18 13:35:43 +0530

    Added api service component and tailwindcss for styling

---
+++ client/src/components/services/api.js
@@ -0,0 +1,41 @@
+import axios from 'axios';
+
+const API_BASE_URL = process.env.REACT_APP_API_BASE_URL;
+const API_TOKEN = process.env.REACT_APP_API_TOKEN;
+const instance = axios.create({baseURL: API_BASE_URL});
+
+instance.interceptors.request.use((config) => {
+    if (token) {
+        config.headers.Authorization = `Bearer ${API_TOKEN}`;
+    }
+    return config;
+});
+
+export const createLogApi = async (logData) => {
+    try {
+        const response = await instance.post('/logs/create', logData);
+        return response;
+    } catch (error) {
+        return error.response;
+    }
+};
+
+export const bulkCreateLogsApi = async (logsData) => {
+    try {
+        const response = await instance.post('/logs/bulkCreate', logsData);
+        return response;
+    } catch (error) {
+        return error.response;
+    }
+};
+
+export const getLogsApi = async (filters) => {
+    try {
+        const response = await instance.get('/logs/get?'+filters);
+        return response;
+    } catch (error) {
+        return error.response;
+    }
+};
+
+export default instance;
